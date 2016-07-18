f15f3789d0f0d707937a99ce8e3b130eec4f40de
nanocloudx <nanocloudx@gmail.com>
2016-07-18 23:19:50 +0900

    fix decompose time

--- libs/decompose-time.js
+++ libs/decompose-time.js
@@ -4,13 +4,23 @@ module.exports = function(time) {
   }
   var timezone = time.substr(8);
   time = time.slice(0, 8).split(':');
-  if (time.length != 3) {
-    return null;
+
+  switch(time.length) {
+    case 2:
+      return {
+        hour: time[0].slice(0, 2),
+        minute: time[1].slice(0, 2),
+        second: '00',
+        timezone: timezone
+      };
+    case 3:
+      return {
+        hour: time[0].slice(0, 2),
+        minute: time[1].slice(0, 2),
+        second: time[2].slice(0, 2),
+        timezone: timezone
+      };
+    default:
+      return null;
   }
-  return {
-    hour: time[0].slice(0, 2),
-    minute: time[1].slice(0, 2),
-    second: time[2].slice(0, 2),
-    timezone: timezone
-  };
 };
